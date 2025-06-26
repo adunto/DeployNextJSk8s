072770346b4067b9b90274b93f1ae670839a048b
Kwon Soonmin <sogno6037@gmail.com>
2025-06-26 17:10:55 +0900

    gitaction test

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,26 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {  
+const nextConfig: NextConfig = {
   /* config options here */
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/bookmarks',
-        permanent: true
-      }
-    ]
+        source: "/",
+        destination: "/bookmarks",
+        permanent: true,
+      },
+    ];
   }, //redirects()
   async rewrites() {
-    const API_BASE_URL = process.env.NEXT_PUBLIC_SERVER_API_BASE_URL
+    const API_BASE_URL = process.env.NEXT_PUBLIC_SERVER_API_BASE_URL;
     return [
-        {
-            source: '/api/:path*',
-            destination: `${API_BASE_URL}/api/:path*`
-            //'http://bookmarker-api-svc:8080/:path*/' // Proxy to Backend
-        }
-    ]
-}, //rewrites  
+      {
+        source: "/api/:path*",
+        destination: `http://bookmarker-api:8080/api/:path*`,
+        //'http://bookmarker-api-svc:8080/:path*/' // Proxy to Backend
+      },
+    ];
+  }, //rewrites
 };
 
 export default nextConfig;
